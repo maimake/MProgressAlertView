aa474c491ba5029e5b834c5c3f3e3b4c268aeb44
mai <yshxinjian@yahoo.com.cn>
2013-06-21 12:16:32 +0800

    add some comments

--- MProgressAlertView/MProgressAlertView.m
+++ MProgressAlertView/MProgressAlertView.m
@@ -54,9 +54,14 @@
 
 -(id)initWithTitle:(NSString *)title message:(NSString *)message delegate:(id)delegate cancelButtonTitle:(NSString *)cancelButtonTitle otherButtonTitles:(NSString *)otherButtonTitles, ...
 {
-    //super init
+    //========================================================
+    //原理：通过调整message的换行，空出一些高度来放置progress views
+    //========================================================
+    
     if(cancelButtonTitle || otherButtonTitles)//有button
         message = message ? [message stringByAppendingString:@"\n\n"] : @"\n";
+    
+    //super init。将固定参数和可变参数传递给父类
     self = [super initWithTitle:title message:message delegate:delegate cancelButtonTitle:cancelButtonTitle otherButtonTitles:otherButtonTitles, nil];
     
     va_list args_list;
@@ -76,7 +81,7 @@
     self.progressView.autoresizingMask = UIViewAutoresizingFlexibleLeftMargin | UIViewAutoresizingFlexibleRightMargin;
     [self addSubview:self.progressView];
     
-    // add progress view
+    // add progress label
     self.progressLabel = [[[UILabel alloc] initWithFrame:CGRectMake(0, 0, 70, 30)] autorelease];
     self.progressLabel.autoresizingMask = UIViewAutoresizingFlexibleLeftMargin | UIViewAutoresizingFlexibleRightMargin;
     self.progressLabel.backgroundColor = [UIColor clearColor];//comment it to look label's position
@@ -105,7 +110,7 @@
     
     CGFloat offset = self.bounds.size.height;
     
-    if (self.numberOfButtons) {//有button
+    if (self.numberOfButtons) {//有button，找到最后一个button，目标将在其偏上20像素
         
         for(UIView* subview in self.subviews)
         {
@@ -115,7 +120,7 @@
         }
         
         offset -= 20;
-    }else{//没有button
+    }else{//没有button，目标将在Alert View底部偏上50像素
         offset -= 50;
     }
     
